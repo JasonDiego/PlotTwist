6647bcdca457f252df05eeb29988b75c7de99c0b
JasonAg <jason.aguilera.s@gmail.com>
2023-02-22 16:12:58 -0700

    API key is fetched from appsettings

--- PlotTwist/Services/PromptService.cs
+++ PlotTwist/Services/PromptService.cs
@@ -6,27 +6,16 @@ namespace PlotTwist.Services
     public class PromptService : IPromptService
     {
         // constructor
-        public PromptService() { _api = new OpenAI_API.OpenAIAPI("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"); }
+        public PromptService(string key) { _api = new OpenAIAPI(key); }
 
         // internal members
         private OpenAIAPI _api;
-        private static PromptService instance;
-
-        // fetch instance
-        public static PromptService Instance
-        {
-            get
-            {
-                instance ??= new PromptService();
-                return instance;
-            }
-        }
 
         // send prompt via OpenAI API
         public async Task<string> SendPrompt()
         {
             // TODO: make prompt dynamic!
-            return await _api.Completions.GetCompletion("Tell me a joke in 5 words.");
+            return await _api.Completions.GetCompletion("Tell me a joke in a few words.");
         }
     }
 }
