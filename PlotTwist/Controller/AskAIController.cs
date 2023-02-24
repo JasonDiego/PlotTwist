cfb004715e9bf61a1d4c850acaf254c16513ec26
JasonAg <jason.aguilera.s@gmail.com>
2023-02-23 17:25:52 -0700

    New controller for interacting with AI

---
+++ PlotTwist/Controller/AskAIController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PlotTwist.Services;
+using Services;
+
+namespace PlotTwist.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AskAIController : ControllerBase
+    {
+        public AskAIController(ILogger<MoviesController> logger, IMovieService movieService, IPromptService promptService)
+        {
+            PromptService = promptService;
+            MovieService = movieService;
+            Logger = logger;
+        }
+
+        public IPromptService PromptService { get; }
+        public IMovieService MovieService { get; }
+        public ILogger<MoviesController> Logger;
+
+        // .../api/askai
+        [HttpGet]
+        public string GetAsync()
+        {
+            string response = Task.Run(() => PromptService.SendPrompt("Give me an enthusiastic 'Hello, World' message.")).Result;
+            return response;
+        }
+    }
+}
