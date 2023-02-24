0b6d8dd7100862f5be907948ab522e1230e4d93e
JasonAg <jason.aguilera.s@gmail.com>
2023-02-23 18:08:07 -0700

    Initialize internal Movies dictionary

--- PlotTwist/Services/JsonFileMovieService.cs
+++ PlotTwist/Services/JsonFileMovieService.cs
@@ -5,21 +5,46 @@ namespace Services
 {
     public class JsonFileMovieService : IMovieService
     {
-        public JsonFileMovieService(IWebHostEnvironment webHostEnvironment) => WebHostEnvironment = webHostEnvironment;
+        // constructor
+        public JsonFileMovieService(IWebHostEnvironment webHostEnvironment)
+        { 
+            WebHostEnvironment = webHostEnvironment;
+
+            try
+            {
+                string JsonFileName = Path.Combine(WebHostEnvironment.WebRootPath, "data", "movies.json");
+
+                // read movies from JSON file
+                using (var jsonFileReader = File.OpenText(JsonFileName))
+                {
+                    var movieList = JsonSerializer.Deserialize<Movie[]>(jsonFileReader.ReadToEnd(),
+                        new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                    if (movieList == null)
+                        throw new ArgumentNullException(nameof(movieList) + " is empty. Check path: " + JsonFileName);
+
+                    // initialize dictionary
+                    int count = 1;
+                    foreach (var movie in movieList)
+                    {
+                        Movies.Add(count++, movie);
+                    }
+                }
+            } catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to read JSON file. Exception: {ex.Message}");
+            }
+        }
 
         public IWebHostEnvironment WebHostEnvironment { get; }
-
-        private string JsonFileName { get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "movies.json"); } }
+        public Dictionary<int, Movie> Movies = new Dictionary<int, Movie>();
 
         public IEnumerable<Movie> GetMovies()
         {
-            using (var jsonFileReader = File.OpenText(JsonFileName))
-            {
-                var movies = JsonSerializer.Deserialize<Movie[]>(jsonFileReader.ReadToEnd(),
-                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-
-                return movies ?? Enumerable.Empty<Movie>();
-            }
+            foreach(var movie in Movies) { Console.WriteLine(movie.Key + ": " + movie.Value); }
+            return Movies.Values;
         }
+
+
     }
 }
